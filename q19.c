344969426a4076b8edc7edd987e3a937022fcd7e
Harshita Pandey <hershey0057@gmail.com>
2026-02-11 23:54:09 +0530

    Add files via upload

---
+++ q19.c
@@ -0,0 +1,31 @@
+/* Problem: Read a string and check if it is a palindrome using two-pointer comparison.
+
+Input:
+- Single line: string s
+
+Output:
+- Print YES if palindrome, otherwise NO
+
+Example:
+Input:
+level
+
+Output:
+YES
+Explanation: String reads same forwards and backwards.   */ 
+
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    char string[50];
+
+    printf("enter string: ");
+    scanf("%c", &string);
+
+    int temp= string;
+
+
+    return 0;
+}
