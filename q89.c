4f199d72f3460a4b5650f54774e19413d6f5e2d5
Harshita Pandey <hershey0057@gmail.com>
2026-04-29 01:18:49 +0530

    q89
    Implement a function to calculate the height of a binary tree based on level-order input.

--- q89.c
+++ q89.c
@@ -1,1 +1,98 @@
-#q89:- 
+/*  Problem Statement:  Find the height (maximum depth) of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+
+Output Format:
+- Print the height of the tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 -1
+
+Output:
+3                  */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) 
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+
+int height(struct Node* root)
+{
+    if(root == NULL)
+        return 0;
+
+    int leftheight = height(root->left);
+    int rightheight = height(root->right);
+
+    return (leftheight > rightheight ? leftheight : rightheight) + 1;
+}
+
+
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+
+
+    // Create nodes array
+    struct Node* nodes[n];
+
+
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+
+
+    // Link nodes (level order)
+    for(int i = 0; i < n; i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    
+
+    int h = height(nodes[0]);
+    printf("%d\n", h);
+    return 0;
+}
