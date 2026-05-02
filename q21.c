b1848909f91fb110ff35adfc4e19734d668224ce
Harshita Pandey <hershey0057@gmail.com>
2026-05-02 10:43:40 +0530

    q21

--- q21.c
+++ q21.c
@@ -1,4 +1,4 @@
-///*
+/*
 Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
 
 Input:
