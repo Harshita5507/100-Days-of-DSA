16516f4d2aef3ad4080fd7b5085a00e20dc9b629
Harshita Pandey <hershey0057@gmail.com>
2026-05-14 20:32:32 +0530

    Add files via upload

---
+++ q161.c
@@ -0,0 +1,61 @@
+/* Problem: Implement Bubble Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64       */
+
+
+#include <stdio.h>
+
+
+int main()
+{
+    int n,i,j,temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+
+    // Bubble Sort Algorithm
+    for(i=0;i<n;i++)
+    {
+        for(j=0; j<n-i-1; j++)
+        {
+            if(arr[j]> arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+
+    printf("Sorted array: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+
+    return 0;
+}
